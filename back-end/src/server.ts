c9b7b1b1f402bbad0e1c9f92e7981f2ec3c76ae9
vtsuellen <vtsuellen@hotmail.com>
2024-02-15 21:35:13 -0300

    fix: conexão banco de dados

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,18 +1,39 @@
-import express from 'express'
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+  'facilitaSystem',
+  'DATABASE_USER',
+  'DATABASE_PASSWORD',
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((error) => {
+    console.error('Unable to connect to the database: ', error);
+  });
+
+import express from 'express';
 
 import { Router, Request, Response } from 'express';
 
 const app = express();
 
-const route = Router()
+const route = Router();
 
-app.use(express.json())
+app.use(express.json());
 
 route.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'hello world with Typescript ' })
-})
+  res.json({ message: 'hello world with Typescript ' });
+});
 
-app.use(route)
+app.use(route);
 
-const PORT = 2122
-app.listen(2122, () => `Server is running on port ${PORT}`)
+const PORT = 2122;
+app.listen(2122, () => `Server is running on port ${PORT}`);
